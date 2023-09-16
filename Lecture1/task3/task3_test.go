b046b2c7e5bcbbc4f320e97a11b87f4f588b5cce
jellyfisharuka <arukeulen@gmail.com>
2023-09-17 01:32:49 +0600

    task3 unit test  update

--- Lecture1/task3/task3_test.go
+++ Lecture1/task3/task3_test.go
@@ -8,7 +8,7 @@ func TestComparingSlice(t *testing.T) {
 	t.Run("Equal Slices", func(t *testing.T) {
 		slice1 := []int{2, 2, 2}
 		slice2 := []int{2, 2, 2}
-		res := comparingSlice(slice1, slice2)
+		res := compareSlice(slice1, slice2)
 		if !res {
 			t.Errorf("Expected comparingSlice(slice1, slice2) to be true, but got false")
 		}
@@ -17,7 +17,7 @@ func TestComparingSlice(t *testing.T) {
 	t.Run("Different Length Slices", func(t *testing.T) {
 		slice1 := []int{2, 2, 2}
 		slice3 := []int{2, 3}
-		res := comparingSlice(slice1, slice3)
+		res := compareSlice(slice1, slice3)
 		if res {
 			t.Errorf("Expected comparingSlice(slice1, slice3) to be false, but got true")
 		}
@@ -26,7 +26,7 @@ func TestComparingSlice(t *testing.T) {
 	t.Run("Different Slices", func(t *testing.T) {
 		slice2 := []int{2, 2, 2}
 		slice3 := []int{2, 3}
-		res := comparingSlice(slice2, slice3)
+		res := compareSlice(slice2, slice3)
 		if res {
 			t.Errorf("Expected comparingSlice(slice2, slice3) to be false, but got true")
 		}
