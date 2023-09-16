55037b98ac2f89220be7106104023dc5efecf57c
jellyfisharuka <arukeulen@gmail.com>
2023-09-17 01:32:09 +0600

    task3 update

--- Lecture1/task3/task3.go
+++ Lecture1/task3/task3.go
@@ -1,6 +1,8 @@
 package main
+
 import "fmt"
-func comparingSlice(sl1 []int, sl2 []int) bool {
+
+func compareSlice(sl1 []int, sl2 []int) bool {
 	if len(sl1) != len(sl2) {
 		return false
 	} else {
@@ -16,6 +18,5 @@ func main() {
 	slice1 := []int{2, 2, 2}
 	slice2 := []int{2, 2, 2}
 	slice3 := []int{2, 3}
-	fmt.Print("comparing result(slice1, slice2): ", comparingSlice(slice1, slice2), " \ncomparing result(slice2, slice3): ", comparingSlice(slice2, slice3), " \ncomparing result(slice1, slice3): ", comparingSlice(slice1, slice3))
+	fmt.Print("comparing result(slice1, slice2): ", compareSlice(slice1, slice2), " \ncomparing result(slice2, slice3): ", compareSlice(slice2, slice3), " \ncomparing result(slice1, slice3): ", compareSlice(slice1, slice3))
 }
-
